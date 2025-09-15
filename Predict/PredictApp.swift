d04c9142649af912816a84e89da094b8bd0370bd
Jeff Gutierrez <jeffgutierrez0524@gmail.com>
2025-09-15 18:23:07 -0400

    Initial Commit

---
+++ Predict/PredictApp.swift
@@ -0,0 +1,17 @@
+//
+//  PredictApp.swift
+//  Predict
+//
+//  Created by Jeff Gutierrez on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct PredictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
